210c920e98f4c9cdaecdb34a33a1365b66cb0ed4
eljandoubi <abdelkarim.eljandoubi@ensta-paris.fr>
2025-07-25 16:13:55 +0200

    refactor rope indexes

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -258,7 +258,6 @@ class RotaryPositionalEmbedding(Module):
 
         self.register_buffer("sin", torch.sin(angles), persistent=False)
         self.register_buffer("cos", torch.cos(angles), persistent=False)
-        self.register_buffer("indexes", torch.arange(d_k).reshape(-1, 2).flip(1).flatten(), persistent=False)
 
     def forward(
         self,
@@ -275,7 +274,48 @@ class RotaryPositionalEmbedding(Module):
         cos = self.cos[token_positions].repeat_interleave(2, -1)
         sin = self.sin[token_positions].repeat_interleave(2, -1)
         sin = sin * (-1) ** torch.arange(1, self.d_k + 1)
-        return cos * x + sin * x[..., self.indexes]
+        index = torch.arange(self.d_k).reshape(-1, 2).flip(1).flatten()
+        return cos * x + sin * x[..., index]
+
+
+def run_multihead_self_attention_with_rope(
+    d_model: int,
+    num_heads: int,
+    max_seq_len: int,
+    theta: float,
+    q_proj_weight: Float[Tensor, " d_k d_in"],
+    k_proj_weight: Float[Tensor, " d_k d_in"],
+    v_proj_weight: Float[Tensor, " d_v d_in"],
+    o_proj_weight: Float[Tensor, " d_model d_v"],
+    in_features: Float[Tensor, " ... sequence_length d_in"],
+    token_positions: Int[Tensor, " ... sequence_length"] | None = None,
+) -> Float[Tensor, " ... sequence_length d_out"]:
+    """
+    Given the key, query, and value projection weights of a naive unbatched
+    implementation of multi-head attention, return the output of an optimized batched
+    implementation. This implementation should handle the key, query, and value projections
+    for all heads in a single matrix multiply.
+    This version of MHA should include RoPE.
+    In this case, the RoPE embedding dimension must be the head embedding dimension (d_model // num_heads).
+    See section 3.2.2 of Vaswani et al., 2017.
+
+    Args:
+        d_model (int): Dimensionality of the feedforward input and output.
+        num_heads (int): Number of heads to use in multi-headed attention.
+        max_seq_len (int): Maximum sequence length to pre-cache if your implementation does that.
+        theta (float): RoPE parameter.
+        q_proj_weight (Float[Tensor, "d_k d_in"]): Weights for the Q projection
+        k_proj_weight (Float[Tensor, "d_k d_in"]): Weights for the K projection
+        v_proj_weight (Float[Tensor, "d_k d_in"]): Weights for the V projection
+        o_proj_weight (Float[Tensor, "d_model d_v"]): Weights for the output projection
+        in_features (Float[Tensor, "... sequence_length d_in"]): Tensor to run your implementation on.
+        token_positions (Int[Tensor, " ... sequence_length"] | None): Optional tensor with the positions of the tokens
+
+    Returns:
+        Float[Tensor, " ... sequence_length d_out"]: Tensor with the output of running your optimized, batched multi-headed attention
+        implementation with the given QKV projection weights and input features.
+    """
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
